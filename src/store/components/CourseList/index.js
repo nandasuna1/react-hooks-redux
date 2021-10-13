ae1331b99c3d807b855214b13047aa2f4a70dc84
nandasuna1 <fbp@ecomp.poli.br>
2021-10-13 16:27:12 -0300

    App Reack hooks + Redux

---
+++ src/store/components/CourseList/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+function addCourseAction(title){
+    return {type: 'ADD_COURSE', title}
+}
+
+export default function CourseList() {
+    const courses = useSelector(state => state.data);
+    const dispatch = useDispatch();
+
+    function addCourse(){
+        dispatch(addCourseAction('new course'))
+    }
+
+    return (
+        <>
+        <ul>
+            {courses.map(course => <li key={course}>{course}</li>)}
+        </ul>
+        <button type='button' onClick={addCourse}>Adicionar Curso</button>
+        </>
+    );
+}
